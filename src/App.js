216cd4d8da6259ce1ec311d83c8b710ae8298237
osamayasin1 <143315816+osamayasin1@users.noreply.github.com>
2024-05-09 22:28:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is my project. Osama Yasin
+          now its ok.
         </p>
         <a
           className="App-link"
