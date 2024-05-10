b9da5a3209335055185b934f25986f21d27e7238
osamayasin1 <143315816+osamayasin1@users.noreply.github.com>
2024-05-10 09:49:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my project. Osama Yasin
+          This is my project. Osama Yasin Websouls
           now its working ok.
         </p>
         <a
